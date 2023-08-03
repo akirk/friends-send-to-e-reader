4ce8a73f2915977c5252b7a135c41381f7e9d1fa
Alex Kirk <akirk@users.noreply.github.com>
2023-08-03 14:07:40 +0200

    Enable unsent posts for any author

--- templates/frontend/ereader/author-header.php
+++ templates/frontend/ereader/author-header.php
@@ -5,28 +5,22 @@
  * @package Friends_Send_To_E_Reader
  */
 
-if ( $args['friend']->has_cap( 'post_collection' ) ) {
-	if ( count( $args['unsent_posts'] ) ) {
-		foreach ( $args['ereaders'] as $id => $ereader ) {
-			?><a class="chip friends-send-new-posts-to-ereader" data-ereader="<?php echo esc_attr( $id ); ?>" data-unsent="<?php echo esc_attr( count( $args['unsent_posts'] ) ); ?>" href="#">
-			<?php
-			echo esc_html(
-				sprintf(
-				// translators: %s is an E-Reader name.
-					_n( 'Send %1$d new post to %2$s', 'Send %1$d new posts to %2$s', count( $args['unsent_posts'] ), 'friends' ),
-					count( $args['unsent_posts'] ),
-					$ereader->get_name()
-				)
-			);
-			?>
-				<i class="form-icon"></i>
-				</a>
-				<?php
-		}
-	} else {
-		?>
-		<span class="chip"><?php esc_html_e( 'No new posts', 'friends' ); ?></span>
+if ( count( $args['unsent_posts'] ) ) {
+	foreach ( $args['ereaders'] as $id => $ereader ) {
+		?><a class="chip friends-send-new-posts-to-ereader" data-ereader="<?php echo esc_attr( $id ); ?>" data-unsent="<?php echo esc_attr( count( $args['unsent_posts'] ) ); ?>" href="#">
 		<?php
+		echo esc_html(
+			sprintf(
+			// translators: %s is an E-Reader name.
+				_n( 'Send %1$d new post to %2$s', 'Send %1$d new posts to %2$s', count( $args['unsent_posts'] ), 'friends' ),
+				count( $args['unsent_posts'] ),
+				$ereader->get_name()
+			)
+		);
+		?>
+			<i class="form-icon"></i>
+			</a>
+			<?php
 	}
 }
 
